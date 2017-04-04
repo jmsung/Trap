2121b123d362db0dcd7c7cdc7f09df394f6ad1e8
DESKTOP-PU99G55\Valelab <jongmin.sung@gmail.com>
2017-04-04 15:10:17 -0700

    Implement Python Integration Toolkit into the Main.vi

---
+++ Trap-Labview/Python_Integration_Toolkit/Trap_Labview_Python.py
@@ -0,0 +1,13 @@
+"""
+Python Integration Toolkit for the Trap-Labview, by Jongmin Sung (4/4/2017)
+
+"""
+
+import numpy as np
+
+def get_mean(x):
+    return np.mean(x)
+    
+def get_size(x):
+    return len(x)
+
